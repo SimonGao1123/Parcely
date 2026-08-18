a1180446da2f88ca2428ec42ed3d83c1216da847
Simon Gao <simongao398@gmail.com>
2026-08-18 02:32:57 -0400

    initial clerk setup alongside cutomized clerk user model for db, TODO setup endpoints, webhooks

---
+++ parcely-backend/accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from common.models import TimestampedModel
+# Create your models here.
+
+# TODO: add stripe integration id's for user after integration is setup
+class AppUser(TimestampedModel):
+    email = models.EmailField(unique=True, db_index=True)
+    clerk_id = models.CharField(max_length=255, unique=True, db_index=True)
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
--- parcely-frontend/app/test/[test]/page.tsx
+++
@@ -1,4 +0,0 @@
-export default async function TestPage({ params }: { params: { test: string } }) {
-  const { test } = await params;
-  return <div>Test Page {test}</div>;
-}
