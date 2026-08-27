1b424c86966ddfd3792c48087bfcec2273c4f071
Simon Gao <simongao398@gmail.com>
2026-08-27 16:06:43 -0400

    updated layouts for storefront prepping for editing route

--- parcely-frontend/app/(storefront)/[storefrontSlug]/(public)/layout.tsx
+++ parcely-frontend/app/(storefront)/[storefrontSlug]/(public)/layout.tsx
@@ -1,5 +1,5 @@
 import { getStorefrontDetails } from "@/lib/api/storefront/getStorefrontDetails";
-import StorefrontNavbar from "../_components/storefront/storefrontNavbar";
+import StorefrontNavbar from "@/app/(storefront)/_components/storefront/storefrontNavbar";
 
 export default async function StorefrontLayout({ children, params }: { children: React.ReactNode, params: Promise<{ storefrontSlug: string }> }) {
     const {storefrontSlug} = await params;
